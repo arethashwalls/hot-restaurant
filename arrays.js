de8560ec597b7b9d52de0421a22f479d81c0224f
Blue Buffalo <anettagoldsher@gmail.com>
2019-01-26 10:52:16 -0700

    basic arrays

---
+++ arrays.js
@@ -0,0 +1,29 @@
+// reserved tables
+var reservedTables = [{
+        name: "John",
+        number: "111-111-1111",
+        email: "aol@aol.com",
+        id: "1"
+    },
+    {
+        name: "Tessa",
+        number: "222-222-2222",
+        email: "gmail@gmail.com",
+        id: "2"
+    },
+];
+
+// wait-listed tables
+var waitlistedTables = [{
+        name: "Greg",
+        number: "333-333-3333",
+        email: "aol@aol.com",
+        id: "3"
+    },
+    {
+        name: "Tessa",
+        number: "444-444-4444",
+        email: "gmail@gmail.com",
+        id: "4"
+    },
+];
